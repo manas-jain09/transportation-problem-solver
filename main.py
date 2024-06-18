f2dc452c245c2f439d50a05d89ee44557155bed9
unknown <vrcharholikar@gmail.com>
2024-06-18 17:40:29 +0530

    Added Vogels Approximation Method Logic

--- main.py
+++ main.py
@@ -270,7 +270,100 @@ class TransportationProblemSolver:
         self.balance_problem(supply, demand, cost_matrix)
 
         allocation = [[0] * len(cost_matrix[0]) for _ in range(len(cost_matrix))]
-        # add logic for vogel_approximation_method
+        new = [[0] * len(cost_matrix[0]) for _ in range(len(cost_matrix))]
+
+        for i in range(len(cost_matrix)):
+            for j in range(len(cost_matrix[0])):
+                allocation[i][j] = 0
+                new[i][j] = cost_matrix[i][j]
+        INF = 10 ** 3
+        n = len(cost_matrix)
+        m = len(cost_matrix[0])
+        ans = 0
+
+        # hepler function for finding the row difference and the column difference
+        def findDiff(grid):
+            rowDiff = []
+            colDiff = []
+            for i in range(len(grid)):
+                arr = grid[i][:]
+                arr.sort()
+                rowDiff.append(arr[1] - arr[0])
+            col = 0
+            while col < len(grid[0]):
+                arr = []
+                for i in range(len(grid)):
+                    arr.append(grid[i][col])
+                arr.sort()
+                col += 1
+                colDiff.append(arr[1] - arr[0])
+            return rowDiff, colDiff
+
+        # loop runs until both the demand and the supply is exhausted
+        while max(supply) != 0 or max(demand) != 0:
+            # finding the row and col difference
+            row, col = findDiff(cost_matrix)
+            # finding the maxiumum element in row difference array
+            maxi1 = max(row)
+            # finding the maxiumum element in col difference array
+            maxi2 = max(col)
+
+            # if the row diff max element is greater than or equal to col diff max element
+            if (maxi1 >= maxi2):
+                for ind, val in enumerate(row):
+                    if (val == maxi1):
+                        # finding the minimum element in grid index where the maximum was found in the row difference
+                        mini1 = min(cost_matrix[ind])
+                        for ind2, val2 in enumerate(cost_matrix[ind]):
+                            if (val2 == mini1):
+                                # calculating the min of supply and demand in that row and col
+                                mini2 = min(supply[ind], demand[ind2])
+                                ans += mini2 * mini1
+                                allocation[ind][ind2] = mini2
+                                # subtracting the min from the supply and demand
+                                supply[ind] -= mini2
+                                demand[ind2] -= mini2
+                                # if demand is smaller then the entire col is assigned max value so that the col is eliminated for the next iteration
+                                if (demand[ind2] == 0):
+                                    for r in range(n):
+                                        cost_matrix[r][ind2] = INF
+                                # if supply is smaller then the entire row is assigned max value so that the row is eliminated for the next iteration
+                                else:
+                                    cost_matrix[ind] = [INF for i in range(m)]
+                                break
+                        break
+            # if the row diff max element is greater than col diff max element
+            else:
+                for ind, val in enumerate(col):
+                    if (val == maxi2):
+                        # finding the minimum element in grid index where the maximum was found in the col difference
+                        mini1 = INF
+                        for j in range(n):
+                            mini1 = min(mini1, cost_matrix[j][ind])
+
+                        for ind2 in range(n):
+                            val2 = cost_matrix[ind2][ind]
+                            if val2 == mini1:
+                                # calculating the min of supply and demand in that row and col
+                                mini2 = min(supply[ind2], demand[ind])
+                                ans += mini2 * mini1
+                                allocation[ind2][ind] = mini2
+                                # subtracting the min from the supply and demand
+                                supply[ind2] -= mini2
+                                demand[ind] -= mini2
+                                # if demand is smaller then the entire col is assigned max value so that the col is eliminated for the next iteration
+                                if (demand[ind] == 0):
+                                    for r in range(n):
+                                        cost_matrix[r][ind] = INF
+                                # if supply is smaller then the entire row is assigned max value so that the row is eliminated for the next iteration
+                                else:
+                                    cost_matrix[ind2] = [INF for i in range(m)]
+                                break
+                        break
+
+            self.output(supply, demand, new, method, allocation)
+
+        return ans
 
     def reset(self):
         self.table_frame.destroy()
